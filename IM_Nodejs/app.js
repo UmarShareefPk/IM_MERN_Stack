ae081547fc7c8bf0a6b0db2560e739edd7724073
Muhammad Umar Shareef <Omarsharif91@gmail.com>
2022-08-10 03:34:07 +0500

    Update app.js

--- IM_Nodejs/app.js
+++ IM_Nodejs/app.js
@@ -14,7 +14,7 @@ var config = require('./config');
 app.use(cors());
 
 // connect to mongodb & listen for requests
-const dbURI= "mongodb+srv://admin:pioneer007@cluster0.dg9t8.mongodb.net/IM?retryWrites=true&w=majority"
+const dbURI= "mongodb+srv://@cluster0.dg9t8.mongodb.net/IM?retryWrites=true&w=majority"
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3333))
   .catch(err => console.log(err));
